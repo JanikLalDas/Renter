bc675f8b9607e6b1d06db9870b4e9ad8f0f6a71e
Janik Lal Das <36584340+JanikLalDas@users.noreply.github.com>
2018-03-28 11:24:57 +0600

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,11 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";
+$password = "";
 $dbname = "BasaVara";
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
